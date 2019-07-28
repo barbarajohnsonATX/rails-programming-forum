d19add1ace3bc4e01797e926896d82cbec0e8a22
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-27 22:04:51 -0500

    Add edit/delete answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,6 @@
 class AnswersController < ApplicationController
+    before_action :find_question, only: [:show, :create, :edit, :update, :destroy]
+    before_action :find_answer, only: [:show, :edit, :update, :destroy]
 
     def new
         @answer = Answer.new
@@ -6,7 +8,7 @@ class AnswersController < ApplicationController
 
     def create
         #answer belongs to question and user
-        @question = Question.find(params[:question_id])
+        #@question = Question.find(params[:question_id]) => before_action
         @answer = @question.answers.build(answer_params)
         @answer.user_id = current_user.id 
 
@@ -19,8 +21,36 @@ class AnswersController < ApplicationController
         redirect_to question_path(@question)
 
     end 
-      
+    
+    def show
+    end 
+
+    def edit 
+    end 
+
+    def update 
+        if @answer.update(answer_params)
+            redirect_to question_path(@question)
+        else 
+            render :edit 
+        end 
+    end 
+
+    def destroy 
+        @answer.destroy
+        redirect_to question_path(@question)
+    end 
+
     private 
+
+    def find_question
+        @question = Question.find(params[:question_id])
+    end 
+
+    def find_answer
+         
+        @answer = @question.answers.find(params[:id])
+    end 
 
     def answer_params
         params.require(:answer).permit(:comment, :user_id, :question_id )

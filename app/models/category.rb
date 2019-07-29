fbda11f3e8e845c31f0cf93c9988b8f6443429c0
Barbara Johnson <johnson.barbara@gmail.com>
2019-07-28 19:04:25 -0500

    Fixed missing > in form-group

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
     has_many :questions
+
+    validates :name, presence: true
+ 
+ 
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,9 @@ class Question < ApplicationRecord
     belongs_to :category
     has_many :answers
 
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :category_id, presence: true
+ 
+
 end

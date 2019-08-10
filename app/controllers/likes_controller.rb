785ebd5e7eb953247004768e70d3d89aaf78877e
Barbara Johnson <johnson.barbara@gmail.com>
2019-08-10 00:05:50 -0500

    Add font awesome

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,15 +4,16 @@ class LikesController < ApplicationController
 
     def create
         if already_liked?
-            flash[:notice] = "You can't like more than once"
+            flash[:notice] = "You already like this."
         else 
-            #question has many likes
+            #create association for a user to like a question
             @question.likes.create(user_id: current_user.id)
             redirect_to question_path(@question)
         end 
     end
 
     def destroy
+        #Unliking
         if !(already_liked?)
           flash[:notice] = "Cannot unlike"
         else
@@ -28,10 +29,12 @@ class LikesController < ApplicationController
     end
 
     def set_like
+        #get the like instance associated with the question
         @like = @question.likes.find(params[:id])
      end
 
      def already_liked?
+        #check if already Like has an entry for user_id for current_user and question_id
         Like.where(user_id: current_user.id, question_id: params[:question_id]).exists?
     end
 end
